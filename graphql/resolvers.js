34d291b6815e4808dcbb0fa9c349420d610cab59
Steven Herrera <47761194+StevenHerrera21@users.noreply.github.com>
2025-10-31 11:32:14 -0500

    feat: agregar mutación en graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -4,6 +4,14 @@ const resolvers = {
     async auditorias() {
       return await AuditLog.find();
     }
-  }
+  },
+  Mutation:{
+    async createAuditoria(_,{input})
+    {
+      const newAuditLog=new AuditLog(input)
+      await newAuditLog.save();
+      return newAuditLog;
+    },
+  },
 };
 module.exports = resolvers;
--- graphql/schema.js
+++ graphql/schema.js
@@ -16,6 +16,19 @@ type Auditoria
     ipOrigen: String
     exitoso: Boolean
 }
+input AuditoriaInput{
+  usuario: String!
+  cedula: String!
+  endpoint: String!
+  metodo: String!
+  fechaHora: String!
+  ipOrigen: String!
+  exitoso: Boolean!
+}
+type Mutation
+{
+  createAuditoria(input:AuditoriaInput):Auditoria
+}
 `;
 module.exports = makeExecutableSchema({
   typeDefs: typeDefs,
